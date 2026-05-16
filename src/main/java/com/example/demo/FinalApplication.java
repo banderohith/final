3f645e2a93b5b49d16c15e2e0723b305bd3c7c6b
banderohith <96186580+banderohith@users.noreply.github.com>
2026-05-16 16:23:51 +0530

    Update FinalApplication.java

--- src/main/java/com/example/demo/FinalApplication.java
+++ src/main/java/com/example/demo/FinalApplication.java
@@ -1,15 +1,15 @@
-//package com.example.demo;
+package com.example.demo;
 
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
-//public class FinalApplication {
+@SpringBootApplication
+public class FinalApplication {
 
-	//public static void main(String[] args) {
-		//SpringApplication.run(FinalApplication.class, args);
-	//}
+	public static void main(String[] args) {
+		SpringApplication.run(FinalApplication.class, args);
+	}
 
-//}
+}
 
 
