66b1442a90481802dde4d8d9381137e9c10f32ce
banderohith <96186580+banderohith@users.noreply.github.com>
2026-05-16 16:10:45 +0530

    Refactor FinalApplication and add HelloController
    Removed commented-out code and added HelloController for a greeting endpoint.

--- src/main/java/com/example/demo/FinalApplication.java
+++ src/main/java/com/example/demo/FinalApplication.java
@@ -1,13 +1,27 @@
+//package com.example.demo;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+//public class FinalApplication {
+
+	//public static void main(String[] args) {
+		//SpringApplication.run(FinalApplication.class, args);
+	//}
+
+//}
+
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class FinalApplication {
+@RestController
+public class HelloController {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FinalApplication.class, args);
-	}
-
+    @GetMapping("/")
+    public String hello() {
+        return "DevOps Project Running Successfully!";
+    }
 }
