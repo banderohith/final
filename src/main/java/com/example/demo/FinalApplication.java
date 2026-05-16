a4ccbf32a35176226f33b9f4b2e43e9e96c23bda
banderohith <96186580+banderohith@users.noreply.github.com>
2026-05-16 16:21:19 +0530

    Update FinalApplication.java

--- src/main/java/com/example/demo/FinalApplication.java
+++ src/main/java/com/example/demo/FinalApplication.java
@@ -12,16 +12,4 @@
 
 //}
 
-package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HelloController {
-
-    @GetMapping("/")
-    public String hello() {
-        return "DevOps Project Running Successfully!";
-    }
-}
