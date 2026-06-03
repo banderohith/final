534b05c8fecbde74f4bc3703716d4ba0347bed23
banderohith <banderohith@karunya.edu.in>
2026-06-03 17:38:32 +0530

    Initial commit for user-service

--- src/main/java/com/example/demo/FinalApplication.java
+++ src/main/java/com/example/demo/FinalApplication.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class FinalApplication {
@@ -9,6 +11,10 @@ public class FinalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FinalApplication.class, args);
 	}
+        @Bean
+        public RestTemplate restTemplate() {
+            return new RestTemplate();
+    }
 
 }
 
--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,11 +2,13 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/")
+    @GetMapping("/users")
     public String home() {
         return "Home Page Running Successfully!";
     }
@@ -24,5 +26,18 @@ public class HelloController {
     @GetMapping("/about-kubernetes")
     public String kubernetes() {
         return "Kubernetes Microservice Running!";
+    }
+    @Autowired
+        private RestTemplate restTemplate;
+
+    @GetMapping("/user-payment")
+    public String userPayment() {
+
+    String response =
+            restTemplate.getForObject(
+                    "http://payment-service:8082/payments",
+                    String.class);
+
+    return "User Service -> " + response;
     }
 }
