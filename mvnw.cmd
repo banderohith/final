080617baf5e83108208dba3c7c69f394b604079c
banderohith <96186580+banderohith@users.noreply.github.com>
2026-07-10 08:50:21 +0530

    Update mvnw.cmd

--- mvnw.cmd
+++ mvnw.cmd
@@ -1,4 +1,4 @@
-<# : batch portion
+<# : batch portio
 @REM ----------------------------------------------------------------------------
 @REM Licensed to the Apache Software Foundation (ASF) under one
 @REM or more contributor license agreements.  See the NOTICE file
